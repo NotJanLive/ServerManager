65603ecc080538de0ee4aca5d065124a16613ea0
NotJan <78273074+NotJanLive@users.noreply.github.com>
2022-06-25 12:48:26 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,92 @@
+import os, requests 
+def get_version(version):
+    if version == "1.8":
+        download(8)
+    elif version == "1.12":
+        download(12)
+    elif version == "1.16":
+        download(16)
+    elif version == "1.18":
+        download(18)
+    elif version == "1.19":
+        download(19);    
+    else:
+        print("Diese Version existiert nicht!")
+        exit()
+
+def download(wanted_version):
+    if wanted_version == 8:
+        os.system("cls")
+        print("Lade nötige Dateien herunter...")
+        response = requests.get("https://cdn.getbukkit.org/spigot/spigot-1.8.8-R0.1-SNAPSHOT-latest.jar")
+        open("server.jar", "wb").write(response.content)
+    elif wanted_version == 12:
+        os.system("cls")
+        print("Lade nötige Dateien herunter...")
+        response = requests.get("https://cdn.getbukkit.org/spigot/spigot-1.12.2.jar")
+        open("server.jar", "wb").write(response.content)  
+    elif wanted_version == 16:
+        os.system("cls")
+        print("Lade nötige Dateien herunter...")
+        response = requests.get("https://cdn.getbukkit.org/spigot/spigot-1.16.5.jar")
+        open("server.jar", "wb").write(response.content)  
+    elif wanted_version == 18:
+        os.system("cls")
+        print("Lade nötige Dateien herunter...")
+        response = requests.get("https://download.getbukkit.org/spigot/spigot-1.18.2.jar")
+        open("server.jar", "wb").write(response.content)
+    elif wanted_version == 19:
+        os.system("cls")
+        print("Lade nötige Dateien herunter...")
+        response = requests.get("https://download.getbukkit.org/spigot/spigot-1.19.jar")
+        open("server.jar", "wb").write(response.content)
+        
+def setup_server(ram):
+    ram = int(ram)
+    os.system("cls")
+    print("Setze alle für dich auf...")
+    start_server(ram)
+    os.system("cls")
+    print("Akzeptiere die EULA für dich")
+    os.remove("eula.txt")
+    with open('eula.txt', 'w') as f:
+        f.write('eula=true')
+        f.close()
+    write_start_file(ram)
+    start_server(ram)
+
+def start_server(ram):
+    os.system(f"java -Xmx{ram}G -Xms{ram}G -jar server.jar nogui PAUSE")
+
+def write_start_file(ram):
+    with open('start.bat', 'w') as f:
+        f.write(f'java -Xmx{ram}G -Xms{ram}G -jar server.jar nogui PAUSE')
+        f.close()
+
+def main():
+    os.system("cls")
+    print("\nAuf welcher Version soll dein Minecraft-Server laufen?")
+    print("""
+##############
+#    1.8     #
+#    1.12    #
+#    1.16    #
+#    1.18    #
+#    1.19    #  
+##############  
+""")
+    version = input(">>>")
+    get_version(version)
+    os.system("cls")
+    print("Wie viel RAM soll dein server haben? (4GB ist empfohlen)")
+    print("""
+#############
+#  1 GB RAM #
+#  2 GB RAM #
+#  4 GB RAM #
+#  8 GB RAM #
+#############
+""")
+    ram = input(">>>")
+    setup_server(ram)
+main()
